5c9b3b2ebe73323ff0c0ed11190e962baaaa6b1b
Nwaokocha Victor <nvonweb@outlook.com>
2023-03-09 02:25:12 -0500

    adds integration test for health check endpoint

--- email-newsletter/src/main.rs
+++ email-newsletter/src/main.rs
@@ -1,23 +1,8 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse};
+//!main.rs
 
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
-}
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
+use email_newsletter::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
-            .route("/health_check", web::get().to(health_check))
-    })
-    .bind("127.0.0.1:8080")?
-    .run()
-    .await
+    run()?.await
 }
